e5c0a797a0addc16a9462bfc681d3bbdd1b93c67
rangdra <rangdra2@gmail.com>
2021-06-29 23:43:18 +0700

    add husky and cache posts

--- pages/api/search.js
+++ pages/api/search.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
   let posts;
 
   if (process.env.NODE_ENV === 'production') {
-    // Fetch from cache
+    posts = require('../../cache/data').posts;
   } else {
     const files = fs.readdirSync(path.join('posts'));
 
